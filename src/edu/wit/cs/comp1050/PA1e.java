9f7f75b1f4bb65f2cc0385f3d6b6fa958af1ebb4
stewartt3 <stewartt3@PF2XTTP1.wit.private>
2022-02-06 21:50:35 -0500

    Homework done

--- src/edu/wit/cs/comp1050/PA1e.java
+++ src/edu/wit/cs/comp1050/PA1e.java
@@ -1,15 +1,9 @@
 package edu.wit.cs.comp1050;
 import java.util.Scanner;
-//TODO: document this class
+//Program that counts the amount of uppercase letters in a string
 public class PA1e {
 	
-	/**
-	 * Counts the number of upper case characters
-	 * within the supplied string
-	 * 
-	 * @param s input string
-	 * @return number of upper case characters
-	 */
+	//Counts the number of upper case characters within the supplied string
 	public static int numUpperCase(String s) {
 		int count = 0;
 		for( int i = 0; i < s.length(); i++) {
@@ -19,12 +13,14 @@ public class PA1e {
 		return count; 
 	}
 
-	// TODO: document this method
+	//Starts the program
 	public static void main(String[] args) {
+		//Gets user inputted string
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a string: ");
 		String s = in.nextLine();
 		int count = numUpperCase(s);
+		//Determines what to print depending on how many uppercase letters there are
 		if(count > 1 )
 			System.out.println("There are " + count + " uppercase letters in the string");
 		else if(count == 0)
